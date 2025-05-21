4cd7d6167e17273a7bdc829b1191cbab348ac21c
Kshitij054 <kshitijpatel54@gmail.com>
2025-05-21 12:18:14 +0530

    added dashboard and feedback

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ import { SignInPage } from "./routes/sign-in";
 import { SignUpPage } from "./routes/sign-up";
 import ProtectRoutes from './layouts/protected-routes';
 import { MainLayout } from './layouts/main-layout';
+import { Generate } from './components/generate';
+import { Dashboard } from './routes/dashboard';
+import { CreateEditPage } from './routes/create-edit-page';
+import { Feedback } from './routes/feedback';
+import { MockLoadPage } from './routes/mock-load-page';
+import { MockInterviewPage } from './routes/mock-interview-page';
+
+
+
 const App = () => {
   return (
     <Router>
@@ -30,6 +39,21 @@ const App = () => {
             </ProtectRoutes>
           }
         >
+        
+        <Route element={<Generate />} path="/generate">
+
+            <Route index element={<Dashboard />} />
+            
+            <Route path=":interviewId" element={<CreateEditPage />} />
+            <Route path="interview/:interviewId" element={<MockLoadPage />} />
+            <Route
+              path="interview/:interviewId/start"
+              element={<MockInterviewPage />}
+            />
+            <Route path="feedback/:interviewId" element={<Feedback />} />
+          </Route>
+
+
 
           
         </Route>
